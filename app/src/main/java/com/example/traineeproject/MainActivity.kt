ddd37ae5cd2a139acbd9eb3a35523ef4749e69a9
Shaparapatah <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>
2022-08-10 01:24:49 +0300

    5. Подключил библиотеку Picasso и ViewBindings

--- app/src/main/java/com/example/traineeproject/MainActivity.kt
+++ app/src/main/java/com/example/traineeproject/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.traineeproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.traineeproject.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
