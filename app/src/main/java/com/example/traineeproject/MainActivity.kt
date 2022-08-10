4d4c988fc587a45f4c194171c79244e2398d378a
Shaparapatah <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>
2022-08-10 23:13:49 +0300

    9. Сделал выводы картинки согласно ДЗ (без кнопки).

--- app/src/main/java/com/example/traineeproject/MainActivity.kt
+++ app/src/main/java/com/example/traineeproject/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+
+
         binding.editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
