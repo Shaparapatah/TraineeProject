6432fb86c4fca542544bcdf039a37c104a7c44b2
Shaparapatah <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>
2022-08-10 22:07:25 +0300

    6. Сделал загрузку картинки по её ссылку 7. Сделал 3-е ДЗ

--- app/src/main/java/com/example/traineeproject/MainActivity.kt
+++ app/src/main/java/com/example/traineeproject/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.traineeproject
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.ImageView
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.traineeproject.databinding.ActivityMainBinding
-import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val imageUrl = "https://picsum.photos/id/870/200/300?grayscale&blur=2"
+    private lateinit var editLink: EditText
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showPicture()
 
     }
+
+    private fun showPicture() {
+        binding.bnt.setOnClickListener {
+            val editLink = binding.editText.text.toString()
+
+            if (editLink != imageUrl) {
+                Toast.makeText(this@MainActivity, "Wrong URL!", Toast.LENGTH_SHORT).show()
+            } else {
+                Glide.with(this@MainActivity)
+                    .load(imageUrl)
+                    .into(binding.imageView)
+            }
+        }
+    }
 }
+
