b46360daaa121c9a268ba8f4ef34fe9a378cd4b6
Shaparapatah <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>
2022-08-14 12:59:12 +0300

    10. Приступил к работе над 4-м домашним заданием(Часы).

--- app/src/main/java/com/example/traineeproject/MainActivity.kt
+++ app/src/main/java/com/example/traineeproject/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.traineeproject
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import com.example.traineeproject.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val imageUrl = "https://picsum.photos/id/870/200/300?grayscale&blur=2"
     private lateinit var binding: ActivityMainBinding
 
 
@@ -20,30 +15,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
-        binding.editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-                val editLink = binding.editText.text.toString()
-
-                if (editLink != imageUrl) {
-                    Toast.makeText(this@MainActivity, "Wrong URL!", Toast.LENGTH_SHORT).show()
-                } else {
-                    Glide.with(this@MainActivity)
-                        .load(imageUrl)
-                        .into(binding.imageView)
-                }
-            }
-        })
     }
 }
 
